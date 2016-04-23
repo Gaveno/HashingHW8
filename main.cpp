e89369e2e61b0acbde51ca5f5ac0e20d939d3372
Gaveno <gavin.monkeystudios@gmail.com>
2016-04-23 10:17:25 -0600

    Implemented Spell Check
    Asks user for file with lists of words. Prints out misspelled words to
    console.

--- main.cpp
+++ main.cpp
@@ -4,11 +4,110 @@
  */
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <unordered_map>
 
 using namespace std;
 
+istream& getWord(istream& fin, string& w)
+{
+    char c;
 
+    w = ""; // clear the string of characters
+
+    while (fin.get(c) && !isalpha(c)); // do nothing. just ignore c
+
+    // return on end-of-file
+    if (fin.eof())
+        return fin;
+
+    // record first letter of the word
+    w += tolower(c);
+
+    // collect letters and digits
+    while (fin.get(c) && (isalpha(c) || isdigit(c)))
+        w += tolower(c);
+
+    return fin;
+}
+
+void spellCheck(string filename) {
+    unordered_map<int, string> words;
+    unordered_map<int, string> dictionary;
+    int keyGen = 0;
+
+    ifstream file;
+    file.open(filename);
+
+    if (file) {
+        // file is open
+        // this is the user defined file
+
+        while (!file.eof()) {
+            string line;
+            getline(file, line);
+            stringstream ss(line);
+            string word;
+            getWord(ss, word);
+
+            // Add word into user list of words
+            words[keyGen] = word;
+            keyGen++;
+        }
+    }
+    else {
+        cout << "Filename: " << filename << " does not exist!" << endl;
+        return;
+    }
+
+    file.close();
+
+    file.open("dict.dat");
+    keyGen = 0;
+
+    if (file) {
+        // file is open
+        // this is the user defined file
+
+        while (!file.eof()) {
+            string line;
+            getline(file, line);
+            stringstream ss(line);
+            string word;
+            getWord(ss, word);
+
+            // Add word into dictionary
+            dictionary[keyGen] = word;
+            keyGen++;
+        }
+    }
+    else {
+        cout << "Filename: " << "dict.dat" << " does not exist!" << endl;
+        return;
+    }
+
+    file.close();
+
+    // Print out misspelled words.
+    for (auto &x: words) {
+        bool correct = false;
+        for (auto &m: dictionary) {
+            if (x.second == m.second) {
+                correct = true;
+            }
+        }
+        if (!correct) {
+            cout << x.second << " is misspelled" << endl;
+        }
+    }
+}
 
 int main() {
-    cout << "Hello World " << endl;
+    string f;
+    cout << "Please enter a file with words to spell check:" << endl;
+    cin >> f;
+
+    spellCheck(f);
 }
